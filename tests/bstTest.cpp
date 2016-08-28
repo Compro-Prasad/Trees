96b7a0e15e1b3b148032ca6ce21d41ba85021092
Compro-Prasad <compro.prasad@gmail.com>
2016-08-28 19:58:01 +0530

    Added tests for removal of nodes from BST

--- tests/bstTest.cpp
+++ tests/bstTest.cpp
@@ -12,14 +12,14 @@ void display(t *start, const t *end, string sep = " ")
 }
 
 template <typename t>
-void test(t a[], size_t size)
+void testAdd(t a[], size_t size)
 {
 	BinarySearchTree<t> tree;
 	t *b = new t[size], *c = new t[size];
 	size_t s = 0;
 
 	tree.add(a, size);
-	tree.putInArrayInInc(c, s);
+	tree.copyToArrIncOrder(c, s);
 
 	copy(a, a + size, b);
 	sort(b, b + size);
@@ -46,6 +46,32 @@ void test(t a[], size_t size)
 	exit(1);
 }
 
+template<typename t>
+void testRemove(t a[], size_t size)
+{
+	BinarySearchTree<t> tree;
+	t *b = new t[size];
+
+	tree.add(a, size);
+
+	copy(a, a + size, b);
+	sort(b, b + size);
+
+	tree.remove(b, size);
+
+	if (!tree.isEmpty())
+	{
+		cout << "Error in removing values\n";
+		cout << "Permutation : ";
+		display(a, a + size);
+		cout << "Sorted Array: ";
+		display(b, b + size);
+		cout << "Tree values : ";
+		tree.print(false);
+		exit(2);
+	}
+}
+
 template <typename t>
 void permute(t a[], size_t k, size_t size, void func(t [], size_t))
 {
@@ -63,13 +89,20 @@ void permute(t a[], size_t k, size_t size, void func(t [], size_t))
 int main()
 {
 	int a[] = {1, 2, 3, 4, 5};
-	permute(a, 0, sizeof(a) / sizeof(int), test);
-	cout << "Tests Passed\n";
+	permute(a, 0, sizeof(a) / sizeof(int), testAdd);
+	cout << "Tests Passed for inserting elements in BST\n";
 	cout << "Conclusions:\n";
 	cout << "      Things working properly:\n";
-	cout << "               BinarySearchTree::add(array, size)";
-	cout << "               BinarySearchTree::add(element)";
-	cout << "               BinarySearchTree::putInArrayInInc(arr, &size)";
-	cout << "               BinarySearchTree::~BinarySearchTree()";
+	cout << "               BinarySearchTree::add(array, size)\n";
+	cout << "               BinarySearchTree::add(element)\n";
+	cout << "               BinarySearchTree::copyToArrIncOrder(arr, &size)\n";
+	cout << "               BinarySearchTree::~BinarySearchTree()\n";
+	permute(a, 0, sizeof(a) / sizeof(int), testRemove);
+	cout << "Tests Passed for removing elements from BST\n";
+	cout << "Conclusions:\n";
+	cout << "      Things working properly:\n";
+	cout << "               BinarySearchTree::remove(array, size)\n";
+	cout << "               BinarySearchTree::remove(element)\n";
+	cout << "               tree::isEmpty()\n";
 	return 0;
 }
